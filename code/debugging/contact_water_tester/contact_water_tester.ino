dcfb586fc9537a70750ae62a39ec45a1aa681bdb
Corinne Smith <67399419+smitty444@users.noreply.github.com>
2022-02-15 16:25:01 -0500

    Create contact_water_tester.ino

---
+++ code/debugging/contact_water_tester/contact_water_tester.ino
@@ -0,0 +1,22 @@
+const int LED = 13;
+const int sensorPin = 41;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+  pinMode(sensorPin, INPUT);
+
+}
+
+void loop() {
+  int state = digitalRead(sensorPin);
+  Serial.print("State: "); Serial.println(state);
+  if(state == HIGH) {
+    digitalWrite(LED, HIGH);
+  }
+  else {
+    digitalWrite(LED, LOW);
+  }
+  delay(500);
+}
