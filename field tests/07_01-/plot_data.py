8dc4c061f667103ee6ef1c5c71d145f01b6ada81
Corinne Smith <67399419+smitty444@users.noreply.github.com>
2022-07-15 16:37:41 -0400

    add 07/01-07/15 field test

---
+++ field tests/07_01-/plot_data.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  1 11:45:46 2022
+
+@author: SmithCA
+"""
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+#import matplotlib.dates as mdates
+from datetime import datetime
+import pandas as pd
+import pylab as pl
+import numpy as np
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+plt.rcParams["font.family"] = "Times New Roman"
+mpl.rcParams.update({'font.size': 14})
+
+import IPython as IP
+IP.get_ipython().magic('reset -sf')
+plt.close('all')
+
+df1 = pd.read_csv('stage_data.csv', parse_dates= {"Datetime" : [4,5]})
+df2 = pd.read_csv('pressure_data.csv', parse_dates={"Datetime" : [4,5]})
+df3 = pd.read_csv('stage_data_filtered.csv', parse_dates= {"Datetime" : [4,5]})
+#df5 = pd.read_csv('pressure_data_filtered.csv', parse_dates={"Datetime" : [3,4]})
+
+plt.figure(figsize=(6.5,3.5))
+plt.plot(df3['Datetime'], df3['stage'], '--', label='ultrasonic sensor')
+plt.plot(df2['Datetime'], df2['stage'], ':', label='pressure sensor')
+#plt.plot(df6['Datetime'], df6['Sensor depth with corrected elevation'], ".-", label = "HOBO sensor")
+plt.legend(framealpha=1)
+plt.xlabel('datetime')
+plt.ylabel('elevation (ft)')
+plt.locator_params(axis="x", nbins=5)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.grid(True)
+#plt.savefig('data.png', dpi=500)
+
+#%% With manual median filter
+
+plt.figure(figsize=(6.5,4.5))
+plt.plot(df3['Datetime'], df3['stage'], '--', label='ultrasonic sensor')
+plt.plot(df2['Datetime'], df2['elevation'], ':', label='pressure sensor')
+plt.legend(framealpha=1)
+plt.xlabel('datetime')
+plt.ylabel('elevation (ft)')
+plt.locator_params(axis="x", nbins=5)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.grid(True)
+plt.savefig('data_filtered.png', dpi=500)
