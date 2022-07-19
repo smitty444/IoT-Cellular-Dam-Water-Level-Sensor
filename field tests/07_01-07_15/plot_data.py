28c279ea6915ba73cacb93941c8d70183edcb691
Corinne Smith <67399419+smitty444@users.noreply.github.com>
2022-07-19 15:00:47 -0400

    rename folder

--- field tests/07_01-07_15/plot_data.py
+++ field tests/07_01-07_15/plot_data.py
@@ -14,8 +14,8 @@ import pylab as pl
 import numpy as np
 import sys
 np.set_printoptions(threshold=sys.maxsize)
-plt.rcParams["font.family"] = "Times New Roman"
-mpl.rcParams.update({'font.size': 14})
+plt.rcParams["font.family"] = "Arial"
+mpl.rcParams.update({'font.size': 12})
 
 import IPython as IP
 IP.get_ipython().magic('reset -sf')
@@ -24,6 +24,7 @@ plt.close('all')
 df1 = pd.read_csv('stage_data.csv', parse_dates= {"Datetime" : [4,5]})
 df2 = pd.read_csv('pressure_data.csv', parse_dates={"Datetime" : [4,5]})
 df3 = pd.read_csv('stage_data_filtered.csv', parse_dates= {"Datetime" : [4,5]})
+df4 = pd.read_csv('rainfall.csv', parse_dates=['date'])
 #df5 = pd.read_csv('pressure_data_filtered.csv', parse_dates={"Datetime" : [3,4]})
 
 plt.figure(figsize=(6.5,3.5))
@@ -41,14 +42,40 @@ plt.grid(True)
 
 #%% With manual median filter
 
-plt.figure(figsize=(6.5,4.5))
-plt.plot(df3['Datetime'], df3['stage'], '--', label='ultrasonic sensor')
-plt.plot(df2['Datetime'], df2['elevation'], ':', label='pressure sensor')
-plt.legend(framealpha=1)
+plt.figure(figsize=(6.5,3.5))
+plt.plot(df3['Datetime'], df3['stage'], '--', color="orange", label='ultrasonic sensor')
+plt.plot(df2['Datetime'], df2['elevation'], ':', color = "green", label='pressure transducer')
+plt.legend(loc="lower right")
 plt.xlabel('datetime')
 plt.ylabel('elevation (ft)')
 plt.locator_params(axis="x", nbins=5)
 plt.xticks(rotation=45)
 plt.tight_layout()
 plt.grid(True)
-plt.savefig('data_filtered.png', dpi=500)
+#plt.savefig('data_filtered.png', dpi=500)
+
+#%% rainfall subplot
+
+fig, a = plt.subplots(figsize=(7.5,3.5))
+
+a.plot(df3['Datetime'], df3['stage'], '--', color="orange", label='ultrasonic sensor')
+a.plot(df2['Datetime'], df2['elevation'], ':', color = "green", label='pressure transducer')
+a.set_ylabel('elevation (ft)')
+a.set_xlabel('datetime')
+a.tick_params('x', rotation=45)
+plt.legend(framealpha=1)
+
+a2 = a.twinx()
+a2.bar(df4['date'], df4['rainfall (in)'], alpha=0.5, align='edge', width=1.0, label='rainfall')
+a2.set_ylabel('rainfall (in)')
+
+plt.legend(framealpha=1)
+
+# plt.xlabel('datetime')
+# a.ylabel('elevation (ft)')
+# a2.ylabel('rainfall (in)')
+plt.locator_params(axis="x", nbins=5)
+#plt.xticks(rotation=45)
+plt.tight_layout()
+plt.grid(True)
+plt.savefig('rainfall.png', dpi=500)
